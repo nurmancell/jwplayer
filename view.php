3f85068911b008011719ef5a883b21a64f482d06
nurmancell <nurmancell@gmail.com>
2016-12-02 18:06:24 +0700

    Update view.php

--- view.php
+++ view.php
@@ -2,13 +2,13 @@
 function view(){
 if (isset($visitor)){
 if ($visitor=="visited")
-include("view.txt");
+include("https://cdn.rawgit.com/nurmancell/jwplayer/master/view.txt");
 } else {
 $file=fopen("view.txt","r+");
-$result=fread($file,filesize("view.txt"));
+$result=fread($file,filesize("https://cdn.rawgit.com/nurmancell/jwplayer/master/view.txt"));
 fclose($file);
 $result += 1;
-$file=fopen("view.txt","w+");
+$file=fopen("https://cdn.rawgit.com/nurmancell/jwplayer/master/view.txt","w+");
 fputs($file,$result);
 fclose($file);
 include("view.txt");
